c466bb3c9b21889f6cfdf7666327769f4db8b9a0
Thiago Sampaio <contatosampaiothiago@gmail.com>
2023-05-09 02:08:08 -0300

    fourth commit

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any ;
+  public eventos : any = [];
+  public eventosFiltrados : any =[];
+    widthImg: number=150;
+  marginImg: number= 2;
+  viewImg : boolean = true;
+  private _filterList: string = '';
 
+  public get filterList(): string{
+    return this._filterList
+  }
+  public set filterList(value:string){
+    this._filterList= value;
+    this.eventosFiltrados = this.filterList ? this.filterEvent(this.filterList) : this.eventos;
+  }
 
 
   constructor(private http: HttpClient) { }
@@ -18,9 +30,25 @@ export class EventosComponent implements OnInit {
   ngOnInit(): void {
     this.geteventos();
   }
+  handleViewImg (){
+    this.viewImg = !this.viewImg;
+  }
+  filterEvent(FiltrarPor: string): any{
+    FiltrarPor = FiltrarPor.toLocaleLowerCase();
+   return this.eventos.filter(
+     (  evento: { tema: string,  local: string} )=>
+     evento.tema.toLocaleLowerCase().indexOf(FiltrarPor) !== -1
+     ||
+     evento.local.toLocaleLowerCase().indexOf(FiltrarPor) !== -1
+
+    )
+  }
   public geteventos () : any{
     this.http.get('https://localhost:7236/api/eventos').subscribe(
-      res => this.eventos = res,
+      res => {
+        this.eventos = res;
+        this.eventosFiltrados=this.eventos;
+      },
       error => console.log(error),
     );
 
